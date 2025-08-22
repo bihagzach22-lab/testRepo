767a1542df84f7152d3b4059e40c7abf6acc60e3
TRAINEE - BIHAG <zabihag@n-pax.com>
2025-08-22 13:18:25 +0800

    2nd exercise

---
+++ third.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Name: ");
+        string name = Console.ReadLine();   
+
+        Console.WriteLine("age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Hi {name} good day");
+        if(age < 18)
+        {
+            Console.WriteLine("minor");
+        }
+        else
+        {
+            Console.WriteLine("ADult");
+        }
+
+
+        for(int i= 1;i <= age; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+
+        Console.WriteLine($"birth day:{getBirth(age)}");
+       
+
+
+    }
+    public static int getBirth(int age)
+    {
+        return 2025 - age;
+    }
+
+}
